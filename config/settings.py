74cc8d43c143b0f9b88b0ecb0cca0ed2f8518879
dilshad-ahmad-actyv <dilshadsoraon09@gmail.com>
2025-01-09 13:26:25 +0530

    upload images into s3 bucket endpoint

--- config/settings.py
+++ config/settings.py
@@ -13,3 +13,9 @@ DATABASE_CONFIG = {
     'Trusted_Connection': 'yes',
     'TrustServerCertificate': 'yes',
 }
+
+# AWS S3 Configuration
+AWS_ACCESS_KEY = os.getenv('AWS_ACCESS_KEY')
+AWS_SECRET_KEY = os.getenv('AWS_SECRET_KEY')
+AWS_BUCKET_NAME = os.getenv('AWS_BUCKET_NAME')
+AWS_REGION = os.getenv('AWS_REGION')
