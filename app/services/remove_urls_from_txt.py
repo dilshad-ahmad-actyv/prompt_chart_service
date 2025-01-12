f3d2fc21564fd157f2d75fb27a6ded120d2d74a6
dilshad-ahmad-actyv <dilshadsoraon09@gmail.com>
2025-01-12 16:29:41 +0530

    add one more condition to remove some extrace string from doc

--- app/services/remove_urls_from_txt.py
+++ app/services/remove_urls_from_txt.py
@@ -1,6 +1,8 @@
 import re
 def remove_urls_line_by_line(text):
     # Define the URL patterns (standard and decomposed URLs)
+    text = re.sub(r'Customer\s*Self\s*Service\s*Portal\.', '', text, flags=re.IGNORECASE)
+
     url_pattern = """
         http[s]?://[^\s\n]+ |                          # Match standard URLs
         (h\s*t\s*t\s*p\s*s?|h\s*t\s*t\s*p)\s*:\s*/\s*/[\w\s\./-]+  # Match decomposed URLs
